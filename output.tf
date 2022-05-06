7436f78c3a8f53a93a36a4d1bd8e82dbf5b58e2f
Alfredo España <alfespa17@gmail.com>
2022-05-06 15:20:30 -0600

    Initial version

---
+++ output.tf
@@ -0,0 +1,4 @@
+output "appservice_id" {
+  value       = azurerm_app_service_plan.appservice.id
+  description = "Azure App Service Plan Id"
+}
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "rg_name" {
+  type    = string
+}
+
+variable "rg_location" {
+  type    = string
+}
+
+variable "appservice_name" {
+  type    = string
+}
+
+variable "appservice_os" {
+  type    = string
+}
+
+variable "appservice_tier" {
+  type    = string
+}
+
+variable "appservice_size" {
+  type    = string
+}
+
+variable "appservice_capacity" {
+  type    = number
+}
